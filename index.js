34918c86a7c89bb8a8b4096fd3d7f3be6a89b654
Ryan Buxton <rbuxton1@gmail.com>
2020-10-08 16:12:35 -0600

    yaml now and map fixed

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const yaml = require('js-yaml');
+const fs = require('fs');
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-var locations = require("./locations.json").locations;
+//var locations = require("./locations.json").locations;
+var locations = yaml.load(fs.readFileSync("./locations.yaml")).locations;
 locations.forEach(function(location){
   app.get(location.url, function(req, res){
     res.render("location.ejs", {location: location});
@@ -12,8 +15,7 @@ locations.forEach(function(location){
 });
 
 app.get("/", function(req, res){
-  var pins = require("./pins.json");
-  res.render("directory.ejs", {locations: locations, pins: pins});
+  res.render("directory.ejs", {locations: locations});
 });
 
 app.listen(5678, function(){ console.log("Started on port 5678"); });
