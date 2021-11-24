92f094d543dcf6157862414c5c2f9417db35273c
Ryan Buxton <rbuxton1@gmail.com>
2021-11-24 13:25:26 -0700

    Update deploy-prod.sh

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,3 +1,3 @@
 docker rm webtour --force
 docker build --tag webtour:latest .
-docker run -p 80:5678 -e VIRTUAL_HOST="webtour.comcastmediatechcenter.org" -e LETSENCRYPT_HOST="webtour.comcastmediatechcenter" -d --name webtour webtour:latest
+docker run -p 80:5678 -e VIRTUAL_HOST="webtour.comcastmediatechcenter.org" -e LETSENCRYPT_HOST="webtour.comcastmediatechcenter" --network=webproxy -d --name webtour webtour:latest
